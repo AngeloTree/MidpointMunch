60129f081ad74c45b7293b81883b78944e7f3ed3
Angelo Chaknis <angelopalmtree@gmail.com>
2023-03-24 16:05:26 -0700

    fix: button was reloading page, since form, added preventDefault

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,28 @@ const address1 = document.querySelector("#address1");
 const address2 = document.querySelector("#address2");
 const findButton = document.querySelector("#findButton");
 
+// Function to fetch the geocoding API result
+function geocodeAddress(apiKey, address) {
+  const encodedAddress = encodeURIComponent(address);
+  const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${apiKey}`;
+
+  return fetch(geocodingApiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.status === "OK") {
+        const location = data.results[0].geometry.location;
+        return { lat: location.lat, lng: location.lng };
+      } else {
+        throw new Error("Geocoding API error: " + data.status);
+      }
+    });
+}
+
 fetch("../config.json")
   .then((response) => {
     if (!response.ok) {
@@ -11,30 +33,26 @@ fetch("../config.json")
   })
   .then((config) => {
     const apiKey = config.apiKey;
-    const address = encodeURIComponent(
-      "1600 Amphitheatre Parkway, Mountain View, CA"
-    );
 
-    const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`;
+    findButton.addEventListener("click", (event) => {
+      event.preventDefault();
 
-    fetch(geocodingApiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (data.status === "OK") {
-          const location = data.results[0].geometry.location;
-          console.log(`Latitude: ${location.lat}, Longitude: ${location.lng}`);
-        } else {
-          console.error("Geocoding API error:", data.status);
-        }
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-      });
+      const address1Value = address1.value;
+      const address2Value = address2.value;
+      console.log("test");
+
+      Promise.all([
+        geocodeAddress(apiKey, address1Value),
+        geocodeAddress(apiKey, address2Value),
+      ])
+        .then(([location1, location2]) => {
+          console.log("Address 1:", location1);
+          console.log("Address 2:", location2);
+        })
+        .catch((error) => {
+          console.error("Geocoding error:", error);
+        });
+    });
   })
   .catch((error) => {
     console.error("Failed to load config.json:", error);
